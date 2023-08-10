fa9cf2294f5429b4e3a4234a37f9c5345f27bec2
nayere <jobdavy64@outlook.com>
2023-08-10 15:58:04 -0700

    Getting started with C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ * main - Entry piont
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+
+	print ("Size of char %d bytes(s)\n", size of(char));
+	print ("Size of int %d bytes(s)\n", size of(int));
+	print ("Size of long int %d bytes(s)\n", size of(long int));
+	print ("Size of float %d bytes(s)\n", size of(float));
+
+	return (0);
+
+}
