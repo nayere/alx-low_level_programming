403024a8fef99a9b388d64a6afd79aed8db0184a
nayere <jobdavy64@outlook.com>
2023-08-11 17:13:45 -0700

    03 alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,21 +4,21 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	int lowerCase = 'a';
-	int upperCase = 'A';
+int lowerCase = 'a';
+int upperCase = 'A';
 
-	while(lowerCase <= 'z')
-	{
-		putchar(lowerCase);
-		lowerCase += 1;
-	}
-	 while(upperCase <= 'Z')
-        {
-                putchar(upperCase);
-                upperCase += 1;
-        }
-	 putchar('\n');
-	 return (0);
+while(lowerCase <= 'z')
+{
+	putchar(lowerCase);
+	lowerCase += 1;
+}
+while(upperCase <= 'Z')
+{
+	putchar(upperCase);
+	upperCase += 1;
+}
+putchar('\n');
+return (0);
 }
