d00a2add02115e9d90de63e98f71662618a00203
nayere <jobdavy64@outlook.com>
2023-08-11 17:40:07 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numb;
+
+while (numb < 10)
+{
+printf("%d", numb);
+numb += 1;
+}
+printf("\n");
+return (0);
+}
