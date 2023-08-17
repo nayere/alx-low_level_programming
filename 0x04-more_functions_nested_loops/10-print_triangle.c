e85a22bf1c20777ccfa9f34bba40a0c4000c9967
nayere <jobdavy64@outlook.com>
2023-08-17 13:02:33 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,19 +10,23 @@ void print_triangle(int size)
 		_putchar('\n');
 	}
 	else
-{
-	int i, j;
-	for (i = 1; i <= size; i++)
 	{
-	`for (j = i; j < size; j++)
-	{
-	_putchar(' ');
-	}
-	for (j = 1; j <= i; j++)
-	{
-	_putchar('#');
-	}
-	_putchar('\n');
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
-}
+
