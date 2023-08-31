cc0e4fac648210788eb34df40a7b72303e846829
nayere <jobdavy64@outlook.com>
2023-08-31 13:51:46 -0700

    0x08-recursion answers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,10 +14,3 @@ int is_prime_number(int n)
 		return (0);
 	return (actual_prime(n, n - 1));
 }
-{
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (actual_prime(n, i - 1));
-}
