6498b90d81d561b625eb568796567c3e57e9f48f
nayere <jobdavy64@outlook.com>
2023-09-22 14:35:18 -0700

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */ 
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
