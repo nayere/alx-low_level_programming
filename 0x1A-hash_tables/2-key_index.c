c44cd635b08ef1fdcfb80b8f5353e5add81bade7
nayere <jobdavy64@gmail.com>
2023-12-22 12:33:32 -0800

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,5 +9,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int hash_results;
+
+	hash_results = hash_djb2(key);
+	if (size != 0)
+		return (hash_results % size);
+	return (0);
 }
