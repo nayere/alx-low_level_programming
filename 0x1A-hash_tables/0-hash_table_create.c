9bd801b366e2247ce8eeacd9267c385b63a27d74
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-12-22 23:27:06 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the hash table
+ * hash_table_create - function that creates a hash table
  *
- * Return: pointer to hash table
+ * Description: function that creates a hash table
+ *
+ * @size: the size of the array
+ *
+ * Return: return NULL or Hash Table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *table;
+	hash_table_t *new_table;
+	unsigned long int index;
 
-if (size == 0)
-return (NULL);
-
-table = calloc(1, sizeof(hash_table_t));
-if (table == NULL)
-  return (NULL);
-  
-  table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
+	new_table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
 	{
-	free(table);
-	return (NULL);
+		free(new_table);
+		return (NULL);
 	}
-	return (table);
+	for (index = 0; index < size; index++)
+		(new_table->array)[index] = NULL;
+	return (new_table);
 }
