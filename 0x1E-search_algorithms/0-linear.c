5ac0acbe1f87a0a6c07e9b8ee6d8a451a6d60eaa
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-04-10 22:14:17 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,3 +1,4 @@
+
 #include "search_algos.h"
 
 /**
@@ -14,10 +15,10 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-size_t i;
+	size_t i;
 
-if (array == NULL)
-	return (-1);
+	if (array == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
